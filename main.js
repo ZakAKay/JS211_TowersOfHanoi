a7f811c6b8f557f60c01ac4c7d4e8b2985af1e39
ZakAKay <zakkay@ymail.com>
2022-07-15 20:59:53 -0500

    final version

--- main.js
+++ main.js
@@ -1,108 +1,148 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-// An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
-
+// An object that represents the three stacks of Towers of Hanoi;
+// * each key is an array of Numbers:
+// * A is the far-left,
+// * B is the middle,
+// * C is the far-right stack
+// * Each number represents the largest to smallest tokens:
+// * 4 is the largest,
+// * 1 is the smallest
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
-  c: []
+  c: [],
 };
 
 // Start here. What is this function doing?
 const printStacks = () => {
-  console.log("a: " + stacks.a);
+  console.log("a: " + stacks.a); 
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
-}
+};
+// `a: ${stacks.a}`
+// STRING INTERPOLATION/ TEMPLATE LITERAL EXAMPLE ABOVE
 
 // Next, what do you think this function should do?
-const movePiece = () => {
+const movePiece = (startStack, endStack) => {
   // Your code here
+let disk = stacks[startStack].pop()
+stacks[endStack].push(disk)
 
-}
+  // pop from startStack
+  // push to endStack
+
+  //user can move "blocks" from column to column
+  //choose the top piece (last value in the array) in the start stack (a b c) pop()
+  //choose the stacl to ,ove the piece to push()
+  //if there's nothing to pop, return error message
+  // if value in startStack array
+};
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
+const isLegal = (startStack, endStack) => {
   // Your code here
-
+if (stacks[startStack].slice(-1) < stacks[endStack].slice(-1) || stacks[endStack] == 0) {
+return true;
+} else { 
+  return false;
 }
+  // let start = startStack
+  // let end = endStack
+
+  //start is a, b, or c
+
+  // if the end stack is empty return true
+
+  // if the last value in the end stack is > start return true
+  // else return false
+
+  // if start == end return false
+};
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // Your code here
-
+if (stacks.c.length ==4) 
+ {
+return true
+} else {
+  return false
+}
+//  if true console log you won
+//start a new game
+// else return false
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
+if (isLegal(startStack, endStack)) {
+  movePiece(startStack, endStack)
+ } else { 
+    return console.log ('That is not a valid move, try again dummy')
+  }
 
-}
+
+  if (checkForWin()) {
+    return console.log("Slow clap* you won.")
+  }
+
+};
 
 const getPrompt = () => {
   printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
+  rl.question("start stack: ", (startStack) => {
+    rl.question("end stack: ", (endStack) => {
       towersOfHanoi(startStack, endStack);
       getPrompt();
     });
   });
-}
+};
 
 // Tests
 
-if (typeof describe === 'function') {
-
-  describe('#towersOfHanoi()', () => {
-    it('should be able to move a block', () => {
-      towersOfHanoi('a', 'b');
+if (typeof describe === "function") {
+  describe("#towersOfHanoi()", () => {
+    it("should be able to move a block", () => {
+      towersOfHanoi("a", "b");
       assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
     });
   });
 
-  describe('#isLegal()', () => {
-    it('should not allow an illegal move', () => {
+  describe("#isLegal()", () => {
+    it("should not allow an illegal move", () => {
       stacks = {
         a: [4, 3, 2],
         b: [1],
-        c: []
+        c: [],
       };
-      assert.equal(isLegal('a', 'b'), false);
+      assert.equal(isLegal("a", "b"), false);
     });
-    it('should allow a legal move', () => {
+    it("should allow a legal move", () => {
       stacks = {
         a: [4, 3, 2, 1],
         b: [],
-        c: []
+        c: [],
       };
-      assert.equal(isLegal('a', 'c'), true);
+      assert.equal(isLegal("a", "c"), true);
     });
   });
-  describe('#checkForWin()', () => {
-    it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+  describe("#checkForWin()", () => {
+    it("should detect a win", () => {
+      stacks = { a: [], b: [], c: [4, 3, 2, 1] };
       assert.equal(checkForWin(), true);
       stacks = { a: [1], b: [4, 3, 2], c: [] };
       assert.equal(checkForWin(), false);
     });
   });
-
 } else {
-
   getPrompt();
-
 }
